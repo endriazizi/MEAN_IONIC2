7382cf6c5a49201297226606131248488500b9d8
Endri Azizi <endri.azizi@gmail.com>
2017-10-25 20:46:45 +0200

    to fisnish

---
+++ server/config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'url': 'mongodb://<endriazizi>:<endriazizi>@ds133465.mlab.com:33465/mean_ionic2'
+}
+
+
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+'user strict'
 var express  = require('express');
 var app      = express();
 var mongoose = require('mongoose');
@@ -5,11 +6,20 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors');
  
-var databaseConfig = require('./config/database');
+// var databaseConfig = require('./config/database');
 var router = require('./app/routes');
  
-mongoose.connect(databaseConfig.url);
- 
+// mongoose.connect(databaseConfig.url);
+//mongoose.connect('mongodb://endriazizi:endriazizi@ds133465.mlab.com:33465/mean_ionic2');
+
+
+
+mongoose.Promise = global.Promise;
+//mongoose.connect('mongodb://localhost:27017/zoo', { useMongoClient: true })
+mongoose.connect('mongodb://endriazizi:endriazizi@ds133465.mlab.com:33465/mean_ionic2', { useMongoClient: true })
+    .then(() => {
+        console.log('connected to mongo database correctly!');
+    })
 app.listen(process.env.PORT || 8080);
 console.log("App listening on port 8080");
  
